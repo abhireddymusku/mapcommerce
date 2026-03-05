b4f40445fb342d7958eaf8abd760a8219c6c8b54
Abhi <abhilash014@gmail.com>
2026-03-05 15:28:03 -0600

    Fix import path for Data360 component

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -2,7 +2,7 @@ import Script from "next/script"
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
-import Data360 from "@/components/data360.tsx"
+import Data360 from "../components/data360"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
