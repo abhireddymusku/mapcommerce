16fbdaad62513a9e80abbf59733aed8bbfa99579
Abhi <abhilash014@gmail.com>
2026-03-05 16:20:30 -0600

    Update data360.tsx

--- storefront/src/components/data360.tsx
+++ storefront/src/components/data360.tsx
@@ -5,7 +5,7 @@ import Script from "next/script"
 export default function Data360() {
   return (
     <Script
-      src="https://cdn.c360a.salesforce.com/beacon/c360a/dccaf7d4-a0e1-4395-9e2b-0a1d89135d34/scripts/c360a.min.js"
+      src="https://cdn.c360a.salesforce.com/beacon/c360a/705e3cec-ac4f-4cc9-81ff-06bd6d14335b/scripts/c360a.min.js"
       strategy="afterInteractive"
       onLoad={() => {
         const s = document.createElement("script")
