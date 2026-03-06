4a2dd65a1c1710a6fd618fb43fb8814026e872ef
Abhi <abhilash014@gmail.com>
2026-03-06 10:47:02 -0600

    Update index.tsx

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -10,13 +10,13 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Well done! You have successfully deployed your Medusa 2.0 store on Railway!
+            Well done! You're here now - This store is to demo the Salesforce Personalization Capabilities!
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Need help customizing your store?
+            Need help customizing the site? Reach out to your VML MAP Team Member
           </Heading>
         </span>
         <a
